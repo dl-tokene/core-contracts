f91eb17a372e523f7ece3b58ae1b4d82f48fd680
Armen Krasotulia <corporateaarakelian@gmail.com>
2022-09-12 19:58:12 +0300

    coments

--- deploy/migrations/901_fullInitAndInject.js
+++ deploy/migrations/901_fullInitAndInject.js
@@ -6,7 +6,9 @@ const Registry = artifacts.require("MasterContractsRegistry");
 const ERC1967Proxy = artifacts.require("ERC1967Proxy");
 const MasterRoleManagement = artifacts.require("MasterRoleManagement");
 
+// keccak256("TOKEN_FACTORY_ADMIN")
 const TOKEN_FACTORY_ADMIN_ROLE = "0xd20e79ee7ab22313b1e35bc08d0608b5faca9822ef8dfa3ee1154eb6d6d13df4";
+// keccak256("CONSTANTS_REGISTRY_ADMIN")
 const CONSTANTS_REGISTRY_ADMIN_ROLE = "0xb70679c2bb63d69954ff974d88551a7613146cf648202f5d0bc5ecdad424d359";
 
 module.exports = async () => {
