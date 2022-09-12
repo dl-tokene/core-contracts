398fcfa86987307e6607efbc7305ce8b7ec040e2
Armen Arakelian <krasotulia@Armens-MacBook-Air.local>
2022-09-12 11:27:57 +0300

    tests for UUPS proxy

--- contracts/RoleManagedRegistry.sol
+++ contracts/RoleManagedRegistry.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.16;
 
 import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
-//import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import "@dlsl/dev-modules/contracts-registry/AbstractContractsRegistry.sol";
 import "./interfaces/IMasterRoleManagement.sol";
 
