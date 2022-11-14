a3bb28aff74d0fe6e3e906f96216c4760c0f4403
Artem Chystiakov <artem.ch31@gmail.com>
2022-11-14 16:51:57 +0200

    fix configs

--- deploy/migrations/1_MasterContractsRegistry.js
+++ deploy/migrations/1_MasterContractsRegistry.js
@@ -3,6 +3,7 @@ const ERC1967Proxy = artifacts.require("ERC1967Proxy");
 
 module.exports = async (deployer) => {
   deployer.startMigrationsBlock = await web3.eth.getBlockNumber();
+
   const registry = await deployer.deploy(Registry);
   await deployer.deploy(ERC1967Proxy, registry.address, []);
 };
