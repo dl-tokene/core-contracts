88fd0bb12a041f26c78006133e3ceba1a20177bb
oleg.komendant <oleg02kom@gmail.com>
2022-11-15 12:53:41 +0200

    Remove useless import

--- contracts/interfaces/core/IConstantsRegistry.sol
+++ contracts/interfaces/core/IConstantsRegistry.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import "@openzeppelin/contracts-upgradeable/access/IAccessControlEnumerableUpgradeable.sol";
-
 interface IConstantsRegistry {
     function addConstant(string calldata key_, bytes calldata value_) external;
 
