07cf7059f301c2e8fc7a98114e914d9f9d78a3e0
kevandee <achayka95@gmail.com>
2022-12-15 19:24:48 +0200

    added Initialized event

--- contracts/interfaces/core/IMasterContractsRegistry.sol
+++ contracts/interfaces/core/IMasterContractsRegistry.sol
@@ -2,6 +2,8 @@
 pragma solidity 0.8.17;
 
 interface IMasterContractsRegistry {
+    event Initialized();
+
     function getMasterAccessManagement() external view returns (address);
 
     function getConstantsRegistry() external view returns (address);
