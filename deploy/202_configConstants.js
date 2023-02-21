d39af863a767dc41b5d0762099680837ad1370de
dovgopoly <idovgopoly@gmail.com>
2023-02-21 12:10:58 +0000

    Type support for ConstantsRegistry

--- deploy/202_configConstants.js
+++ deploy/202_configConstants.js
@@ -25,15 +25,43 @@ module.exports = async (deployer, logger) => {
       throw new Error(`Constant ${constant} is incorrectly specified`);
     }
 
-    const constantEncodedValue = web3.eth.abi.encodeParameters([constantType], [constantValue]);
-
-    if (constantEncodedValue == "0x") {
-      throw new Error(`Consant ${constant} value is empty`);
+    if (constantType == "bytes" && constantValue == "0x") {
+      throw new Error(`Constant ${constant} value is empty`);
     }
 
-    logger.logTransaction(
-      await constantsRegistry.addConstant(constant, constantEncodedValue),
-      `Added constant ${constant} with value ${constantEncodedValue}`
-    );
+    switch (constantType) {
+      case "bytes":
+        logger.logTransaction(
+          await constantsRegistry.addBytes(constant, constantValue),
+          `Added constant ${constant} with value ${constantValue}`
+        );
+        break;
+      case "string":
+        logger.logTransaction(
+          await constantsRegistry.addString(constant, constantValue),
+          `Added constant ${constant} with value ${constantValue}`
+        );
+        break;
+      case "uint256":
+        logger.logTransaction(
+          await constantsRegistry.addUint256(constant, constantValue),
+          `Added constant ${constant} with value ${constantValue}`
+        );
+        break;
+      case "address":
+        logger.logTransaction(
+          await constantsRegistry.addAddress(constant, constantValue),
+          `Added constant ${constant} with value ${constantValue}`
+        );
+        break;
+      case "bytes32":
+        logger.logTransaction(
+          await constantsRegistry.addBytes32(constant, constantValue),
+          `Added constant ${constant} with value ${constantValue}`
+        );
+        break;
+      default:
+        throw new Error(`Constant ${constant} has invalid type ${constantType}`);
+    }
   }
 };
